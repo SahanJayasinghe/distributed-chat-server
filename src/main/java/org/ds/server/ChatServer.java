0dd85bd1069b11fe6f88548afe74e63467478add
SahanJayasinghe <47143531+SahanJayasinghe@users.noreply.github.com>
2022-03-09 22:56:53 +0530

    read server config file

--- src/main/java/org/ds/server/ChatServer.java
+++ src/main/java/org/ds/server/ChatServer.java
@@ -1,18 +1,27 @@
 package org.ds.server;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
+//import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class ChatServer {
-    int port;
+    private String serverId = null;
+    private int port;
     private ServerSocket serverSocket;
+    private HashMap<String, HashMap<String, String>> serverConfig;
 
-    public ChatServer(int port) {
-        this.port = port;
+    public ChatServer() {
+        serverConfig = new HashMap<>();
     }
 
-    public void start() {
+    public void start(String[] args) {
         try {
+            serverId = args[0].strip();
+            readConfigFile(args[1].strip());
             serverSocket = new ServerSocket(this.port);
             System.out.println("Server is listening on port " + port);
             ServerState.init("MainHall-s1");
@@ -25,6 +34,28 @@ public class ChatServer {
         }
     }
 
+    private void readConfigFile(String configPath) {
+        File configFile = new File(configPath);
+        try {
+            Scanner fileReader = new Scanner(configFile);
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] data = line.strip().split("\\t");
+                HashMap<String, String> currentConfig = new HashMap<>();
+                currentConfig.put("address", data[1]);
+                currentConfig.put("clientsPort", data[2]);
+                currentConfig.put("coordPort", data[3]);
+                serverConfig.put(data[0], currentConfig);
+                if (serverId.equals(data[0])) {
+                    port = Integer.parseInt(data[2]);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
     public void stop() {
         try {
             serverSocket.close();
@@ -34,14 +65,15 @@ public class ChatServer {
     }
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Syntax: java Server <port-number>");
+        if (args.length < 2) {
+            System.out.println("Syntax: java Server <server-id> <\"config-path\">");
             System.exit(0);
         }
 
-        int port = Integer.parseInt(args[0]);
+//        int port = Integer.parseInt(args[0]);
+//        System.out.println(args.length);
 
-        ChatServer server = new ChatServer(port);
-        server.start();
+        ChatServer server = new ChatServer();
+        server.start(args);
     }
 }
