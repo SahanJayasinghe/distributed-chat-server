b5db06da8d4c447983660a59cbd553e70387e7ac
KaveeshaSIlva <47806601+KaveeshaSIlva@users.noreply.github.com>
2022-03-22 13:01:14 +0530

    heartbeat - removed listner

--- src/main/java/org/ds/server/HeartBeatScheduler.java
+++ src/main/java/org/ds/server/HeartBeatScheduler.java
@@ -8,62 +8,37 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.nio.charset.StandardCharsets;
 import java.io.DataOutputStream;
+import java.io.IOException;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class HeartBeatScheduler {
 
-    private HashMap<String, Long> heartbeatReceivedTimes;
-    private static Long HEARTBEAT_INTERVEL = 500L;
+    private static HashMap<String, Long> heartbeatReceivedTimes;
+    private static Long HEARTBEAT_INTERVEL = 1000L;
     private static Long TIMEOUT = HEARTBEAT_INTERVEL * 3;
     private String serverId = null;
-    private Integer serverPort = null;
-    public String HEARTBEAT = "HEARTBEAT";
+    public static String HEARTBEAT = "HEARTBEAT";
     public String SERVER_DOWN = "SERVER_DOWN";
     HashMap<String, HashMap<String, String>> serverConfig;
-    private JSONParser jsonParser;
-    private static String leader = null;
 
-    public HeartBeatScheduler(HashMap<String, HashMap<String, String>> serverConfig, String _serverId, String _leader) {
+    public static void updateHeartbeatReceivedTimes(String server_id) {
+        System.out.println("Heartbeat recieved from : " + server_id);
+        heartbeatReceivedTimes.put(server_id, System.nanoTime());
+        System.out.println(heartbeatReceivedTimes);
+    }
+
+    public HeartBeatScheduler(HashMap<String, HashMap<String, String>> _serverConfig, String _serverId) {
         serverId = _serverId;
-        jsonParser = new JSONParser();
+        System.out.println("Heartbeat scheduler initialized - server ID : " + serverId);
+        heartbeatReceivedTimes = new HashMap<>();
+        serverConfig = _serverConfig;
         for (String server_Id : serverConfig.keySet()) {
             heartbeatReceivedTimes.put(server_Id, 0L);
         }
-        serverPort = Integer.parseInt(serverConfig.get(serverId).get("coordPort"));
-        leader = _leader;
-    }
 
-    class HeartBeatListner extends Thread {
-        public void run() {
-            while (true) {
-                ServerSocket serverSocket;
-                try {
-                    serverSocket = new ServerSocket(serverPort);
-                    Socket socket = serverSocket.accept();
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-                    String inputLine = in.readLine();
-                    System.out.println("Received: " + inputLine);
-                    if (inputLine != null) {
-
-                        JSONObject request = (JSONObject) jsonParser.parse(inputLine);
-                        String msgType = (String) request.get("type");
-                        if (msgType.equals(HEARTBEAT)) {
-                            String server_id = (String) request.get("server");
-
-                            heartbeatReceivedTimes.put(server_id, System.nanoTime());
-                        }
-                    }
-                    in.close();
-                    socket.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
     }
 
     class HeartBeatValidator extends Thread {
@@ -75,33 +50,23 @@ public class HeartBeatScheduler {
                     e.printStackTrace();
                 }
                 Long now = System.nanoTime();
+                String leader = ServerConnectionManager.getLeader();
                 Set<String> serverIds = heartbeatReceivedTimes.keySet();
                 for (String server_Id : serverIds) {
-                    Long lastHeartbeatReceivedTime = (Long) heartbeatReceivedTimes.get(server_Id);
-                    Long timeSinceLastHeartbeat = now - lastHeartbeatReceivedTime;
-                    if (timeSinceLastHeartbeat >= HEARTBEAT_INTERVEL) {
-                        // InformLeader(serverId);
-                        if (server_Id == leader) {
-                            // send leader elect msg
-                        } else {
-                            int port = Integer.parseInt(serverConfig.get(leader).get("coordPort"));
-                            String address = serverConfig.get(leader).get("address");
-                            Socket socket;
-                            try {
-                                socket = new Socket(address, port);
-                                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                                JSONObject msg = new JSONObject();
-                                msg.put("type", SERVER_DOWN);
-                                msg.put("server", server_Id);
-                                out.write((msg.toJSONString() + "\n").getBytes(StandardCharsets.UTF_8));
-                                out.flush();
-                                out.close();
-                                socket.close();
-                            } catch (Exception e) {
-                                e.printStackTrace();
+                    if (!serverId.equals(server_Id)) {
+                        Long lastHeartbeatReceivedTime = (Long) heartbeatReceivedTimes.get(server_Id);
+                        Long timeSinceLastHeartbeat = now - lastHeartbeatReceivedTime;
+                        if (timeSinceLastHeartbeat >= HEARTBEAT_INTERVEL * 1000000) {
+                            if (server_Id.equals(leader)) {
+                                System.out.println("leader failed");
+                                // send leader elect msg
+                            } else {
+                                System.out.println("server failed - " + server_Id);
+                                // notify leader
                             }
                         }
                     }
+
                 }
             }
         }
@@ -110,9 +75,11 @@ public class HeartBeatScheduler {
     class HeartBeatSender extends Thread {
         public void run() {
             while (true) {
-                Set<String> serverIds = heartbeatReceivedTimes.keySet();
+                // Set<String> serverIds = heartbeatReceivedTimes.keySet();
+                Set<String> serverIds = ServerConnectionManager.getOnlineServers();
+                System.out.println("online servers " + serverIds);
                 for (String server_Id : serverIds) {
-                    if (server_Id != serverId) {
+                    if (!server_Id.equals(serverId)) {
                         int port = Integer.parseInt(serverConfig.get(server_Id).get("coordPort"));
                         String address = serverConfig.get(server_Id).get("address");
                         Socket socket;
@@ -123,6 +90,7 @@ public class HeartBeatScheduler {
                             msg.put("type", HEARTBEAT);
                             msg.put("server", serverId);
                             out.write((msg.toJSONString() + "\n").getBytes(StandardCharsets.UTF_8));
+                            System.out.println("Heartbeat sent! to :" + server_Id);
                             out.flush();
                             out.close();
                             socket.close();
@@ -144,10 +112,8 @@ public class HeartBeatScheduler {
     }
 
     public void start() {
-        HeartBeatListner listner = new HeartBeatListner();
         HeartBeatSender sender = new HeartBeatSender();
         HeartBeatValidator validator = new HeartBeatValidator();
-        listner.start();
         sender.start();
         validator.start();
     }
